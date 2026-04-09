1f74a27c5aed86cd9639d93bcecf90bb2ca45f3b
fuqingrong <fqr001101@163.com>
2026-04-10 00:52:35 +0800

    refactor: 删除冗余的 MemoryStorage/MemoryEntrypoint/factory，修复 compressor _encoding 残留

--- qrclaw/memory/storage/__init__.py
+++ qrclaw/memory/storage/__init__.py
@@ -2,18 +2,11 @@
 记忆存储模块
 
 包含：
-- MemoryStorage: 文件存储操作
 - MemoryIndexer: 延迟更新索引管理器（Dirty Flag 模式）
 """
 
-from qrclaw.memory.storage.storage import MemoryStorage
-from qrclaw.memory.storage.indexer import (
-    MemoryIndexer,
-    create_indexer_node,
-)
+from qrclaw.memory.storage.indexer import MemoryIndexer
 
 __all__ = [
-    "MemoryStorage",
     "MemoryIndexer",
-    "create_indexer_node",
 ]
